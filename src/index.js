f21a547ee2baca551a090fb55d2ca2a3ab8710b2
resident7709 <resident7709@gmail.com>
2023-07-06 08:42:33 +0300

    ..setting-classes-and-text-conditionally

--- src/index.js
+++ src/index.js
@@ -77,11 +77,18 @@ function Menu() {
       <h2>Our menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map(pizza => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <React.Fragment>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious!!
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map(pizza => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </React.Fragment>
       ) : (
         <p>We're still working on our menu. Please comeback later..🙂</p>
       )}
